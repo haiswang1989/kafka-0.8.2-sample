cf3bc74d3879a75a31d82219544e5ce66b3cfc49
hansen.wang <hansen.wang@vipshop.com>
2017-12-20 17:23:54 +0800

    add code

---
+++ src/main/java/com/kafka10/consumer/Kafka10Consumer.java
@@ -0,0 +1,47 @@
+package com.kafka10.consumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Kafka10Consumer {
+    
+    private static final String TOPIC = "HelloKafka";
+    
+    public static void main(String[] args) {
+        
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.102:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group_kafka10_2");
+        
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true"); //offset自动提交
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"); //offset自动提交时间间隔
+        
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000"); //comsumer的session的超时时间
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        
+        
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> consumerRecord : records) {
+                System.out.println(consumerRecord);
+            }
+        }
+        
+        
+        
+//        consumer.close();
+//        System.out.println("over...");
+    }
+
+}
---
+++ src/main/java/com/kafka11/producer/Kafka11Producer.java
@@ -0,0 +1,56 @@
+package com.kafka11.producer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class Kafka11Producer {
+
+    private static final String TOPIC = "HelloKafka11";
+    
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        
+        Properties properties = new Properties();
+        //broker集群的地址
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.103:9092");
+        //key的序列化方式
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        //value的序列化方式
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        boolean isSync = true;
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, "Hello kafka");
+        System.out.println("Send : " + record);
+        if(isSync) {
+            //同步的方式,注意这边同步的方式,需要调用一下Future的get()方法,不然不会立即发送
+            producer.send(record).get();
+        } else {
+            //异步的方式
+            producer.send(record, new ProducerCallback());
+        }
+        
+        producer.close();
+    }
+}
+
+
+/**
+ * 异步回调
+ * <p>Description:</p>
+ * @author hansen.wang
+ * @date 2017年12月20日 上午9:42:29
+ */
+class ProducerCallback implements Callback {
+
+    @Override
+    public void onCompletion(RecordMetadata arg0, Exception arg1) {
+        System.out.println("RecordMetadata : " + arg0);
+        System.out.println("Exception : " + arg1);
+    }
+}
